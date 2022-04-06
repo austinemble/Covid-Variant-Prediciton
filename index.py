511bbda25f4723243449f8e7a885d4779b64cc7d
Austin Emmanuval Noble <72311538+austinemble@users.noreply.github.com>
2022-04-06 16:33:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import streamlit as st;
 # import plotly.express as px;
 import pandas as pd;
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import dataSeperation;
 
 dataSeperation.dataSeperate()
